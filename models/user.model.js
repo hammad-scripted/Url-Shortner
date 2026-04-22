ad525bfdc55da32c1da4044e244530824b148b79
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-22 16:11:34 +0530

    design schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,12 @@
-import { pgTable, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, varchar, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users', {
   id: uuid().primaryKey().defaultRandom(),
+  firstname: varchar('first_name', { length: 60 }).notNull(),
+  lastname: varchar('last_name', { length: 60 }),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: text().notNull(),
+  salt: text().notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
